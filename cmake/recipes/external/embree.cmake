8bb08fa46c5c76c9e05e237408ff55716597cf4a
hynking <hynkinglin@gmail.com>
2024-01-30 23:33:25 +0800

    set embree version 3.13.5 to use c++20

--- cmake/recipes/external/embree.cmake
+++ cmake/recipes/external/embree.cmake
@@ -8,7 +8,7 @@ include(FetchContent)
 FetchContent_Declare(
     embree
     GIT_REPOSITORY https://github.com/embree/embree.git
-    GIT_TAG        v3.13.3
+    GIT_TAG        v3.13.5
     GIT_SHALLOW    TRUE
 )
 
